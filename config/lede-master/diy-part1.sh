f7f8c2d1a0857d6f05d6926cd432c970895d26d4
Cloud 1024 <42521985+itq5@users.noreply.github.com>
2024-07-25 21:53:31 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -10,19 +10,11 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-rm -rf package/lean/luci-theme-argon && echo "已删除 luci-theme-argon"
-rm -rf package/lean/ddns-scripts_aliyun && echo "已删除 ddns-scripts_aliyun"
-rm -rf package/lean/ddns-scripts_dnspod && echo "已删除 ddns-scripts_dnspod"
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
 git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone https://github.com/linkease/istore.git package/istore
-git clone https://github.com/sirpdboy/sirpdboy-package.git package/opentopd
-git clone https://github.com/linkease/nas-packages.git package/nas
-git clone https://github.com/linkease/nas-packages-luci.git package/nas_luci
-# git clone https://github.com/kiddin9/openwrt-packages.git package/kiddin9
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+# echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;master' >> feeds.conf.default
