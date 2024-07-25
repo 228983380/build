6e7b6fdd9c266bb6746dd67e548e3df03dd56cb0
Cloud 1024 <42521985+itq5@users.noreply.github.com>
2024-07-25 14:28:52 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -17,7 +17,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
-git clone https://github.com/messense/aliyundrive-webdav package/messense
 git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
