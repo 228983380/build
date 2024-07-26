2b961fafb0528ab20784ed76af30c427892c7ad0
Cloud 1024 <42521985+itq5@users.noreply.github.com>
2024-07-26 21:35:04 +0800

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -12,3 +12,11 @@
 # other
 # rm -rf package/utils/{ucode,fbtest}
 
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
+git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;master' >> feeds.conf.default
